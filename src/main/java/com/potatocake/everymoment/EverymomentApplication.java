5e3a81d2c0dcdb471063e22db878d977396f2acd
JunHyeongChoi <ajfeh22@gmail.com>
2024-09-15 14:18:53 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/potatocake/everymoment/EverymomentApplication.java
@@ -0,0 +1,13 @@
+package com.potatocake.everymoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverymomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverymomentApplication.class, args);
+	}
+
+}
