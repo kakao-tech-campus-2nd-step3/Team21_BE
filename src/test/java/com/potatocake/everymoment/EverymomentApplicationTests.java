11d6fcf4c7e619df14e1269e41282b9a5d37b9e9
JunHyeongChoi <ajfeh22@gmail.com>
2024-10-26 20:06:13 +0900

    fix: ci 테스트 오류 해결

--- src/test/java/com/potatocake/everymoment/EverymomentApplicationTests.java
+++ src/test/java/com/potatocake/everymoment/EverymomentApplicationTests.java
@@ -2,7 +2,9 @@ package com.potatocake.everymoment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("prod")
 @SpringBootTest
 class EverymomentApplicationTests {
 
